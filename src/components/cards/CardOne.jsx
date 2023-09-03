611612c0ed123e75122815706166acc4922d5c52
Semetery29 <mdazizulhakimsuny@gmail.com>
2023-09-03 11:12:12 +0600

    Three Industry Banner added

--- src/components/cards/CardOne.jsx
+++ src/components/cards/CardOne.jsx
@@ -1,7 +1,18 @@
 const CardOne = () => {
   return (
-    <div>
-      <h1>One</h1>
+    <div className="hero p-32 md:max-h-[1290] bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img src="industry-fintech.svg" className="w-1/2 rounded-lg " />
+        <div>
+          <h1 className="text-5xl font-bold">Box Office News!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/cards/CardTwo.jsx
+++ src/components/cards/CardTwo.jsx
@@ -1,7 +1,18 @@
 const CardTwo = () => {
   return (
-    <div>
-      <h1>Two</h1>
+    <div className="hero p-32 md:max-h-[1290] bg-base-200">
+      <div className="hero-content flex-col lg:flex-row">
+        <img src="industry-iot.webp" className="w-1/2 rounded-lg" />
+        <div>
+          <h1 className="text-5xl font-bold">Box Office News!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
     </div>
   );
 };
